aafdc00d75cb51f492e2fd0816b21cee485d1736
Dave Cohen <dave@project768.com>
2012-04-09 17:09:32 +0200

    added process-user-choice function, which splits input args and passes them on to specific action processing functions.

--- src/zombiecity/mainloop.clj
+++ src/zombiecity/mainloop.clj
@@ -4,3 +4,10 @@
 
 ;; create worldgrid and buildings
 (generate-world)
+
+
+
+;; PLAYER LOOP
+(view-currentlocation)
+(println "What do you do next?")
+(process-user-choice (read-line))
--- src/zombiecity/prototype.clj
+++ src/zombiecity/prototype.clj
@@ -121,17 +121,23 @@
 
   )
 
+(defn process-user-choice
+  "Takes a string argument (from read-line) and dispatches to different actions based on that. TODO: make multifn?"
+  [playerchoice]
+  (let [splitchoice (map #(.toLowerCase %) (.split playerchoice " "))
+        choice (first splitchoice)
+        args (rest splitchoice)] ;; ("args" "in" "a" "list")
+  (cond
+   (= choice "look") (view-currentlocation)
+   (= choice "move") "dostuff"
+  )))
+
+
 ;; navigation function -- being able to move along a street
 
 
 ;; player model
 ;;;;;current location, inventory, skills, health, energy, weight-capacity
-
-
-;; narration model
-;;;;;;proper description of surroundings (buildings, furniture, items)
-;;;;;;proper description of streets (which direction they go)
-
 
 
 
