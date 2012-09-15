59ed94d959303f7e0cbc031b357c78ca07a6c9f7
Dave Cohen <dave@project768.com>
2012-09-15 11:27:49 +0200

    added eclipse files to .gitignore, tiny changes

--- test/zombiecity/test/core.clj
+++ test/zombiecity/test/core.clj
@@ -1,5 +1,12 @@
 (ns zombiecity.test.core
   (:use [zombiecity.core])
+  (:use [zombiecity.userinput])
+  (:use [zombiecity.mainloop])
+  (:use [zombiecity.data.buildings])
+  (:use [zombiecity.data.rooms])
+  (:use [zombiecity.items])
+  (:use [zombiecity.data.furniture])
+  
   (:use [clojure.test]))
 
 (deftest replace-me ;; FIXME: write
